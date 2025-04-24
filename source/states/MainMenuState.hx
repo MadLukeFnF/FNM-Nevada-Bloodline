eb5d8e146b4e5e9fa9f94927ef5569a4a656a7e3
MadLukeFnF <scpf539@gmail.com>
2025-04-24 17:24:55 -0300

    Add files via upload
    IM TIRED ASF!!!

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -17,7 +17,6 @@ import states.editors.MasterEditorMenu;
 import options.OptionsState;
 import openfl.Lib;
 
-var char:FlxSprite;
 
 class MainMenuState extends MusicBeatState
 {
@@ -230,33 +229,6 @@ class MainMenuState extends MusicBeatState
 		virtualPad.cameras = [camHUD];
         
 		super.create();
-		switch (FlxG.random.int(1, 2)) //You can change it with any number if there are more
-		{
-		case 1:
-			char = new FlxSprite(500, 500).loadGraphic(Paths.image('mainmenu/Boy'));//Thanks to EIT for the tutorial
-			char.frames = Paths.getSparrowAtlas('mainmenu/Boy');
-			char.animation.addByPrefix('Idle', 'Idle', framerate, true); //the bool value's for deciding if it loops or not
-			char.animation.play('Idle');
-			char.scrollFactor.set();
-			char.flipX = false; //You should have already animated it in the right position in Animate
-			char.antialiasing =ClientPrefs.globalAntialiasing;
-			FlxG.sound.play(Paths.sound('sound'), 2); //optional
-			add(char);
-		}
-		
-		{
-		case 2:
-		
-			char = new FlxSprite(500, 500).loadGraphic(Paths.image('mainmenu/GF'));//Thanks to EIT for the tutorial
-			char.frames = Paths.getSparrowAtlas('mainmenu/GF');
-			char.animation.addByPrefix('Idle', 'Idle', framerate, true); //the bool value's for deciding if it loops or not
-			char.animation.play('Idle');
-			char.scrollFactor.set();
-			char.flipX = false; //You should have already animated it in the right position in Animate
-			char.antialiasing =ClientPrefs.globalAntialiasing;
-			FlxG.sound.play(Paths.sound('sound'), 2); //optional
-			add(char);
-		}
 	}
 	
 	var canClick:Bool = true;
